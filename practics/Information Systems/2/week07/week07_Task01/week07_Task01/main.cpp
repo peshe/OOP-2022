32192f8a788ff40bcc591f94783c16192abd88b1
AlexPetrov <49147077+AlexDays1@users.noreply.github.com>
2022-04-04 19:15:11 +0300

    Update main.cpp

--- practics/Information Systems/2/week07/week07_Task01/week07_Task01/main.cpp
+++ practics/Information Systems/2/week07/week07_Task01/week07_Task01/main.cpp
@@ -6,22 +6,5 @@
 
 int main()
 {
-	Dictionary d;
-	char inputWord[50];
-	char inputMeaning[50];
-
-	std::cin.getline(inputWord, 50);
-	std::cin.getline(inputMeaning, 50);
-
-	d.addRecord(inputWord, inputMeaning);
-
-	std::cin.getline(inputWord, 50);
-	std::cin.getline(inputMeaning, 50);
-
-	d.addRecord(inputWord, inputMeaning);
-
-	Record fin = d.find("apple");
-
-	fin.printWord();
 	return 0;
 }
